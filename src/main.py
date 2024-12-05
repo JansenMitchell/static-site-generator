220b9dfb6785c59c2af47bf980793d928ac8cf58
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-05 16:18:53 -0600

    Add TextNode class and main function to demonstrate usage

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import *
+
+def main():
+    test_object = TextNode("This is a text node", TextType.NORMAL_TEXT, "https://www.boot.dev")
+    print(test_object)
+    
+main()
