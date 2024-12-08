3eb726cebcaaefe8a23a73075e5649bf34400e91
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-08 14:54:38 -0600

    Add equality tests for TextNode class attributes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,16 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
         
+    def test_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.text, node2.text)   
+        
+    def test_eq_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.text_type, node2.text_type)
+        
     def test_none(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertIsNone(node.url)
