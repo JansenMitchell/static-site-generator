7078b1c1f36785a8d4eedcaf26449da6f49d9bad
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-10 15:33:39 -0600

    Add unit tests for HTMLNode class to validate properties and initialization

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,32 @@ import unittest
 from htmlnode import *
 
 class TestHTMLNode(unittest.TestCase):
-    pass
+    def test_none(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+        
+    def test_not_none(self):
+        node = HTMLNode("tag", "value", "children", "props")
+        self.assertIsNotNone(node.tag)
+        self.assertIsNotNone(node.value)
+        self.assertIsNotNone(node.children)
+        self.assertIsNotNone(node.props)
+        
+    def test_props_equal(self):
+        node = HTMLNode(None, None, None, 
+                        {
+                            "href": "https://www.google.com", 
+                            "target": "_blank",
+                        })
+        node2 = HTMLNode(None, None, None, 
+                        {
+                            "href": "https://www.google.com", 
+                            "target": "_blank",
+                        })
+        self.assertDictEqual(node.props, node2.props)
     
 if __name__ == "__main__":
     unittest.main() 
