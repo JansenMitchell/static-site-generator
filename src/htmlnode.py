12eac3e10339164ba8c93d60ccc8996ddf5132f0
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-11 15:29:35 -0600

    Add LeafNode class with HTML rendering functionality

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,3 +13,18 @@ class HTMLNode:
     
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+       def __init__(self, tag, value, props=None):
+           super().__init__(tag, value, [], props=None)
+           
+           def to_html(self):
+               if self.value == None:
+                   raise ValueError("Leaf node must have a value.")
+               if self.tag == None:
+                   return self.value
+               #TODO: Loop through self.props dict to get the needed values.
+               if self.props != None:
+                   return f"<{self.tag}>{self.value}</{self.tag}>\n<a href=<{self.props["href"]}</a>"
+               return f"<{self.tag}>{self.value}</{self.tag}>"
+           
