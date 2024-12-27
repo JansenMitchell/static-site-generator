87dec9d770f21cc9aca93ad31673e9db409862d5
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-27 11:36:29 -0600

    Add split_nodes_delimiter function and corresponding unit tests for TextNode splitting

---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,16 @@
+import unittest
+
+from textnode import *
+from splitnodesdelimiter import split_nodes_delimiter
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is a text with a `code block` word", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes,
+                        [
+                            TextNode("This is text with a ", TextType.NORMAL),
+                            TextNode("code block", TextType.CODE),
+                            TextNode(" word", TextType.NORMAL),
+                        ])
+        
