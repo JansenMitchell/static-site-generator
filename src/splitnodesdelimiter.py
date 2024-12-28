2b3a8ce79f10e9dd17e9bb513e472ce8fd9469c5
JansenMitchell <jansenmitchellp@gmail.com>
2024-12-28 13:15:32 -0600

    Implement split_nodes_delimiter function to segment TextNodes based on a specified delimiter

--- src/splitnodesdelimiter.py
+++ src/splitnodesdelimiter.py
@@ -1,4 +1,9 @@
 from textnode import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    for node in old_nodes:
+        if node.text_type == TextType.NORMAL:
+            segements = node.text.split(delimiter)
+            for segment in segements:
+                segment = TextNode(text, text_type)
+                
