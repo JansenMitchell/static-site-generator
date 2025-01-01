25692e68e65b022640df5d4e8fa7ae95a308c3cc
JansenMitchell <jansenmitchellp@gmail.com>
2025-01-01 15:13:12 -0600

    Implement extract_markdown_images and extract_markdown_links functions to parse image and link syntax from markdown text

--- src/extractmarkdown.py
+++ src/extractmarkdown.py
@@ -1,13 +1,7 @@
 import re
 
-text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-
 def extract_markdown_images(text):
-    results = []
-    alt_text = re.findall(r"\[(.*?)\]", text)
-    url = re.findall(r"\((.*?)\)", text)
-    #TODO: Get the result to look like the following:
-    # # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
-    pass
+    return re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
 
-print(extract_markdown_images(text))
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
