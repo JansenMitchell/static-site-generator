8170bab59825c03b2569c2911fd4491536753a83
JansenMitchell <jansenmitchellp@gmail.com>
2025-01-24 17:25:27 -0600

    Implement markdown_to_blocks function to split markdown into blocks based on double newlines

--- src/splitblocks.py
+++ src/splitblocks.py
@@ -1,8 +1,6 @@
 import re
 
 def markdown_to_blocks(markdown):
-    # When the function detects a new line after a string, it splits.
-    # Leading and trailing whitespace is stripped from each block.
-    # "Empty" blocks are removed. 
-    # Empty blocks are blocks that are empty with an empty block preceding and succeding it.
-    pass
+    # TODO: Strip leading and trailing whitespace.
+    blocks = markdown.split("\n\n")
+    return blocks
