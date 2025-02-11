be5c6a824b8751075d6d460f5aad717aeee129ab
JansenMitchell <jansenmitchellp@gmail.com>
2025-02-11 15:08:57 -0600

    Implement recursive directory copy functionality in copy_source_to_destination

--- src/main.py
+++ src/main.py
@@ -8,3 +8,13 @@ def copy_source_to_destination(source, destination):
             print(f"Directory '{destination}' and its contents have been removed.")
         except Exception as e:
             print(f"Error removing '{destination}': {e}")
+    os.mkdir(destination)
+    
+    #TODO: Add logging
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+        elif os.path.isdir(source_path):
+            copy_source_to_destination(source_path, dest_path)
