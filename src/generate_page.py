c5e0e23a0605af3e585c345aee92de68870e812e
JansenMitchell <jansenmitchellp@gmail.com>
2025-02-22 12:48:37 -0600

    Add initial content for "The Unparalleled Majesty of The Lord of the Rings" and update index link; enhance page generation with recursive function

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,6 +1,7 @@
 from markdowntohtml import markdown_to_html_node
 from htmlnode import *
 import os
+import pathlib
 
 def extract_title(markdown):
     markdown_split = markdown.split('\n')
@@ -30,3 +31,14 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, "w") as file:
         file.write(final_html)
+        
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    print(f"Generating page from {dir_path_content}, to {dest_dir_path} using {template_path}")
+    
+    for entry in os.listdir(dir_path_content):
+        full_path = os.path.join(dir_path_content, entry)
+        if os.path.isfile(full_path):
+            if entry.endswith(".md"):
+                pass
+        elif os.path.isdir(full_path):
+            pass
